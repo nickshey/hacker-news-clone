98eea8279874faf04f130598f2c35127db3391c1
Taylor Rohrich <taylorrohrich@gmail.com>
2017-06-27 13:02:18 -0400

    added styling

---
+++ app/src/Jobs.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+class Jobs extends Component {
+  render() {
+    return <div>Jobs </div>;
+  }
+}
+export default Jobs;
---
+++ app/src/Show.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+class Show extends Component {
+  render() {
+    return <div>Show</div>;
+  }
+}
+export default Show;
